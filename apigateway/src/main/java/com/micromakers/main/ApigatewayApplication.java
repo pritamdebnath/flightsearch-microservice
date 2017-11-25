19bcc3f68cd47d379a8d099ed7a127d301d4e721
Pritam Debnath <pritamdebnath9@gmail.com>
2017-11-25 20:24:04 +0530

    some restructuring and token check bug fix

--- apigateway/src/main/java/com/micromakers/main/ApigatewayApplication.java
+++ apigateway/src/main/java/com/micromakers/main/ApigatewayApplication.java
@@ -1,4 +1,4 @@
-package com.example.main;
+package com.micromakers.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- flightsearch/src/main/java/com/micromakers/main/FlightsearchApplication.java
+++ flightsearch/src/main/java/com/micromakers/main/FlightsearchApplication.java
@@ -3,12 +3,16 @@ package com.micromakers.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 @EnableEurekaClient
+@EnableResourceServer
 public class FlightsearchApplication {
 
     public static void main(String[] args) {
@@ -18,5 +22,10 @@ public class FlightsearchApplication {
     @RequestMapping("/test")
     public String test() {
         return "you are in flight search app";
+    }
+    
+    @RequestMapping("/api/test")
+    public String test2(@RequestHeader HttpHeaders headers) {
+        return "protected ";
     }
 }
