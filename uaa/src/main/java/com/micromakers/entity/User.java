00d59e2592da840ee71ae6b702625769a794017c
Pritam Debnath <pritamdebnath9@gmail.com>
2017-11-25 21:09:00 +0530

    adding uaa project

---
+++ uaa/src/main/java/com/micromakers/entity/User.java
@@ -0,0 +1,58 @@
+package com.micromakers.entity;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String username;
+	private String password;
+	@OneToMany(fetch=FetchType.EAGER,cascade=CascadeType.ALL)
+	private List<Role> roles;
+	
+	
+	public User(){
+		
+	}
+	public User(Long id, String username, String password, List<Role> roles) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.roles = roles;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public List<Role> getRoles() {
+		return roles;
+	}
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
+}
